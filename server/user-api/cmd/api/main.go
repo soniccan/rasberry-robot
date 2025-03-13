daf3bc63188359b2563983436ebeb93d19d4b966
soniccan <shunsuke.baba@so-tech.co.jp>
2025-03-14 04:26:17 +0900

    ハンドラーの設定

---
+++ server/user-api/cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("HelloWorld"))
+	})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("サーバー起動に失敗しました:", err)
+	}
+}
