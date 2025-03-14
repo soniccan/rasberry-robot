98b27af6899f230efbdbe788c689c209d2c5d021
soniccan <shunsuke.baba@so-tech.co.jp>
2025-03-14 13:17:51 +0900

    dotenvの設定

--- server/user-api/go.mod
+++ server/user-api/go.mod
@@ -1,3 +1,8 @@
 module user-api
 
 go 1.24.1
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
--- server/user-api/main.go
+++ server/user-api/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	godotenv.Load()
+	token := os.Getenv("CLOUDRUN_ACCESS_TOKEN")
+	if token == "" {
+		log.Fatal("CLOUDRUN_ACCESS_TOKENが設定されていません")
+	}
+
+	cloudRunURL := os.Getenv("CLOUDRUN_URL")
+
+	req, err := http.NewRequest("GET", cloudRunURL, nil)
+	if err != nil {
+		log.Fatalf("リクエスト作成エラー: %v", err)
+	}
+	// AuthorizationヘッダーにBearerトークンを設定
+	req.Header.Set("Authorization", "Bearer "+token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatalf("リクエスト実行エラー: %v", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("レスポンス読み込みエラー: %v", err)
+	}
+
+	log.Printf("レスポンスステータス: %s", resp.Status)
+	log.Printf("レスポンス本文: %s", string(body))
 }
